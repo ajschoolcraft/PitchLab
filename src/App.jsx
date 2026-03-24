af35205bc06ef3a7256deea5e73953720d9ad4e5
joaquincrespo37 <joaquincrespo37@gmail.com>
2026-03-24 16:30:25 -0400

    Merge team changes and rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
 import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
+import { ToastProvider } from './components/Toast'
 import ProtectedRoute from './components/ProtectedRoute'
 import NavBar from './components/NavBar'
 import Landing from './pages/Landing'
 import Auth from './pages/Auth'
 import Dashboard from './pages/Dashboard'
 import Record from './pages/Record'
-import ScriptGeneratorV2 from './pages/ScriptGeneratorV2'
+import ScriptGenerator from './pages/ScriptGenerator'
 
 function App() {
   return (
     <AuthProvider>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
-        <Route path="/record" element={
-          <ProtectedRoute>
-            <Record />
-          </ProtectedRoute>
-        } />
-        <Route path="/script-generator" element={
-          <ProtectedRoute>
-            <ScriptGeneratorV2 />
-          </ProtectedRoute>
-        } />
-      </Routes>
+      <ToastProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/record" element={
+            <ProtectedRoute>
+              <Record />
+            </ProtectedRoute>
+          } />
+          <Route path="/script-generator" element={
+            <ProtectedRoute>
+              <ScriptGenerator />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </ToastProvider>
     </AuthProvider>
   )
 }
